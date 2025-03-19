ca7fd9cb8ef1901b5135762f733f91239b4403f9
julio <anezjulio>
2025-03-18 21:10:52 -0300

    cometarios en la clase main

--- src/Principal.java
+++ src/Principal.java
@@ -3,5 +3,9 @@ package src;
 public class Principal {
     public static void main(String[] args) {
         System.out.println("marico el que lo lea");
+        System.out.println("y el que lo vea tambien");
+
+
+
     }
 }
