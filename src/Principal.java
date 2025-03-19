5e120df3984a9e706e6d164662f1048eafc7895d
luisfunp <guillermo.december.1993@gmail.com>
2025-03-18 17:23:31 -0700

    nos caimos a conazos pero resuelto todo bien

--- src/Principal.java
+++ src/Principal.java
@@ -4,8 +4,6 @@ public class Principal {
     public static void main(String[] args) {
         System.out.println("marico el que lo lea");
         System.out.println("y el que lo vea tambien");
-
-
-
+        System.out.println("Marico el Que lo Escribio");
     }
 }
